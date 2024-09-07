c6f8e78828e6f719086c10131d077bd24c05e743
Nicolas Brusco <nicolasbrusco@hotmail.com>
2024-09-07 16:40:13 -0300

    chore: install initial project dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,71 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="Vite/client"/>
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import autoAlias from 'vite-plugin-auto-alias'
+import dynamicImport from 'vite-plugin-dynamic-import'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+// @ts-expect-error: Could not find a declaration file for module 'vite-plugin-obfuscator'
+import { viteObfuscateFile } from 'vite-plugin-obfuscator'
+
+const obfuscator_options = {
+  compact: true,
+  controlFlowFlattening: false,
+  deadCodeInjection: false,
+  debugProtection: false,
+  debugProtectionInterval: 0,
+  disableConsoleOutput: false,
+  identifierNamesGenerator: 'hexadecimal',
+  log: false,
+  numbersToExpressions: false,
+  renameGlobals: false,
+  selfDefending: false,
+  simplify: true,
+  splitStrings: false,
+  stringArray: true,
+  stringArrayCallsTransform: false,
+  stringArrayCallsTransformThreshold: 0.5,
+  stringArrayEncoding: [],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 1,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 2,
+  stringArrayWrappersType: 'variable',
+  stringArrayThreshold: 0.01,
+  unicodeEscapeSequence: false
+}
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 5174
+  },
+  plugins: [
+    react(),
+    autoAlias({
+      mode: 'sync',
+      prefix: '#'
+    }),
+
+    ViteMinifyPlugin({}),
+    viteObfuscateFile(obfuscator_options),
+    dynamicImport()
+  ],
+  build: {
+    target: 'esnext',
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules'))
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+        }
+      }
+    }
+  }
 })
