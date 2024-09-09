e4fb58676901058e2befc69f54d5194ab0453a8f
Nicolas Brusco <nicolasbrusco@hotmail.com>
2024-09-09 17:41:22 -0300

    deps: uninstall vite obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -5,37 +5,6 @@ import { defineConfig } from 'vite'
 import autoAlias from 'vite-plugin-auto-alias'
 import dynamicImport from 'vite-plugin-dynamic-import'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
-// @ts-expect-error: Could not find a declaration file for module 'vite-plugin-obfuscator'
-import { viteObfuscateFile } from 'vite-plugin-obfuscator'
-
-const obfuscator_options = {
-  compact: true,
-  controlFlowFlattening: false,
-  deadCodeInjection: false,
-  debugProtection: false,
-  debugProtectionInterval: 0,
-  disableConsoleOutput: false,
-  identifierNamesGenerator: 'hexadecimal',
-  log: false,
-  numbersToExpressions: false,
-  renameGlobals: false,
-  selfDefending: false,
-  simplify: true,
-  splitStrings: false,
-  stringArray: true,
-  stringArrayCallsTransform: false,
-  stringArrayCallsTransformThreshold: 0.5,
-  stringArrayEncoding: [],
-  stringArrayIndexShift: true,
-  stringArrayRotate: true,
-  stringArrayShuffle: true,
-  stringArrayWrappersCount: 1,
-  stringArrayWrappersChainedCalls: true,
-  stringArrayWrappersParametersMaxCount: 2,
-  stringArrayWrappersType: 'variable',
-  stringArrayThreshold: 0.01,
-  unicodeEscapeSequence: false
-}
 
 export default defineConfig({
   server: {
@@ -48,7 +17,6 @@ export default defineConfig({
     }),
 
     ViteMinifyPlugin({}),
-    viteObfuscateFile(obfuscator_options),
     dynamicImport()
   ],
   build: {
